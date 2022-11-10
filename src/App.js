1b25e4b56a93c63ea65f6c243a1b6ccf1f63a121
claytonmayfield <clayton_mayfield@insideranken.org>
2022-11-10 15:20:05 -0600

    bmi calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import React,  { useState } from 'react';
+
 
 function App() {
+
+const [calc, setCalc] = useState("")
+const [answer, setAnswer] = useState("")
+
+const ops = ["+", "-", "*", "/", "."]
+const updateCalc  = (value) => {
+  if ((ops.includes(value) && (calc === "")) || 
+  (ops.includes(value) && (ops.includes(calc.slice)(-1)))
+  ){
+    return
+  }
+  
+    setCalc(calc + value)
+  
+    if(!ops.includes(value))
+    {
+      setAnswer(eval(calc + value).tostring())
+    }
+}
+
+const calculate = () => {
+  setCalc(eval(calc).tostring())
+}
+
+const deleteChar = () => {
+  if(calc === "")
+  {
+    return
+  }
+  const value = calc.slice(0, -1)
+  setCalc(value)
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='display'>
+        {answer ? <span>{answer}</span>: ""}&nbsp;nbsp;
+        {calc || "0"}
+        <div className='operators'>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+
+          <button onClick={deleteChar}>DEL</button>
+        </div>
+        <div className='digits'>
+        <button onClick={() => updateCalc("1")}>1</button>
+        <button onClick={() => updateCalc("2")}>2</button>
+        <button onClick={() => updateCalc("3")}>3</button>
+        <button onClick={() => updateCalc("4")}>4</button>
+        <button onClick={() => updateCalc("5")}>5</button>
+        <button onClick={() => updateCalc("6")}>6</button>
+        <button onClick={() => updateCalc("7")}>7</button>
+        <button onClick={() => updateCalc("8")}>8</button>
+        <button onClick={() => updateCalc("9")}>9</button>
+        <button onClick={() => updateCalc("0")}>0</button>
+        <button onClick={() => updateCalc(".")}>.</button>
+
+        <button onClick={calculate}>=</button>
+        </div>
+      </div>
     </div>
-  );
+    </div>
+  );    
 }
 
 export default App;
